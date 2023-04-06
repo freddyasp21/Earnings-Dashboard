27cf9408568264ffa6c531f70582c2eaa85b9683
freddyasp21 <freddyperozo21@gmail.com>
2023-04-06 00:21:24 -0400

    update

--- src/assets/components/Sidebar.jsx
+++ src/assets/components/Sidebar.jsx
@@ -1,11 +1,16 @@
 import React from "react";
-import { RiHome4Line,RiSuitcaseLine, RiBook3Line } from "react-icons/ri";
+import {
+  RiHome4Line,
+  RiSuitcaseLine,
+  RiBook3Line,
+  RiBarChart2Line,
+} from "react-icons/ri";
 
 function Sidebar() {
   return (
     <div className="bg-indigo-900 text-white h-full overflow-y-scroll">
       {/* Contenedor superior */}
-      <div className="flex justify-center p-8 flex-col items-center gap-2">
+      <div className="flex justify-center p-8 flex-col items-center gap-2 h-[30vh]">
         <img
           src="https://img.freepik.com/foto-gratis/hombre-moreno-positiva-brazos-cruzados_1187-5797.jpg?w=740&t=st=1680751510~exp=1680752110~hmac=ff99bd4cb8e109c82ac374579a3da4e6fa4fdafc0e7d854b68249a6352b11a2e"
           alt=""
@@ -16,20 +21,28 @@ function Sidebar() {
       </div>
 
       {/*Contenedor inferior  */}
-      <nav className="bg-indigo-600 p-8 rounded-tr-full">
-        <a className="flex">
-          <RiHome4Line /> Home
-        </a>
-        <a className="">
-            <RiSuitcaseLine/> Projects
-        </a>
-        <a className="">
-            <RiBook3Line/> Invoices
-        </a>
-        <a className=""></a>
-      </nav>
+      <div className="bg-indigo-600 p-8 rounded-tr-[100px] h-[70vh] overflow-y-scroll flex flex-col justify-between gap-8">
+        <nav className="">
+          <a className="flex items-center gap-5 text-xl py-2 px-4 rounded-xl hover:bg-indigo-900/50 cursor-pointer transition-colors">
+            <RiHome4Line /> Home
+          </a>
+          <a className="flex items-center gap-5 text-xl py-2 px-4 rounded-xl hover:bg-indigo-900/50 cursor-pointer transition-colors">
+            <RiSuitcaseLine /> Projects
+          </a>
+          <a className="flex items-center gap-5 text-xl py-2 px-4 rounded-xl hover:bg-indigo-900/50 cursor-pointer transition-colors">
+            <RiBook3Line /> Invoices
+          </a>
+          <a className="flex items-center gap-5 text-xl py-2 px-4 rounded-xl hover:bg-indigo-900/50 cursor-pointer transition-colors">
+            <RiBarChart2Line /> Reports
+          </a>
+        </nav>
+        <div className="bg-indigo-900/50 p-4 rounded-xl">
+            <p className="">Having troubles?</p>
+            <a className="font-semibold cursor-pointer">Contact Us</a>
+        </div>
+      </div>
     </div>
-  ); 
+  );
 }
 
 export default Sidebar;
