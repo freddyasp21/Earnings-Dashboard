0dd01b1ecc5bbcc3269e1826d8cc8cac7860cb99
freddyasp21 <freddyperozo21@gmail.com>
2023-04-05 23:09:06 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Sidebar from './assets/components/Sidebar'
+
+function App() {
+  return (
+    <div className=''>
+      <Sidebar/>
+    </div>  
+  )
+}
+
+export default App
